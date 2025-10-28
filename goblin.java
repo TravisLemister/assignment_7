b2cb2df502a54dcf5df5388016c84794c06035e0
TravisLemister <tlemister@icloud.com>
2025-10-28 11:15:31 -0400

    Update goblin.java

--- goblin.java
+++ goblin.java
@@ -1,2 +1,9 @@
 public class Goblin extends Enemy{
-  
+   public Wizard(int h, int d) {
+        super(h, d);
+   }
+  @Override
+  void attack(){
+    System.out.println("The goblin gobbles");
+  }
+}
