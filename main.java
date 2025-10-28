8df44ae558bda9e74aad344f4e8bb16c2be48c1d
TravisLemister <tlemister@icloud.com>
2025-10-28 12:16:40 -0400

    Update main.java

--- main.java
+++ main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
     enemy e1 = new enemy (100 , 10);
-        System.out.println(e1.getHealth);
+        System.out.println(e1.health);
+        System.out.println(e1.damage);
+
+    enemy e2 = new wizard (115, 25, "fire");
+        System.out.println(e2.health);
+        System.out.println(e2.damage);
+        System.out.println(e2.type);
